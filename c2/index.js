fdef963384b39d3d50d834ba61af5ba73f758e23
Viktor Jordanoski <viktorj560@gmail.com>
2023-09-07 21:30:39 +0200

    feat: class 2 - 2nd task

--- c2/index.js
+++ c2/index.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 //read file
 const readData = (source) => {
-    return new Promise( (success, fail) => {
-        fs.readFile(`${source}.json`, "utf-8", (err, data) =>{
+    return new Promise((success, fail) => {
+        fs.readFile(`${source}.json`, "utf-8", (err, data) => {
             if(err) return fail(err);
             const out = JSON.parse(data);
             return success(out);
@@ -39,3 +39,64 @@ const writeData = (data, destination) => {
 //1.addPerson 
 //2.updatePerson
 //3.removePerson
+
+const addPerson = async(id, firstName, lastName) => {
+    try{
+        const person = {
+            id: id,
+            firstName: firstName,
+            lastName: lastName
+        };
+        let data = await readData("./persons");
+        data.push(person);
+        await writeData(data, "./persons");
+    }catch(err){
+        throw err;
+    }
+};
+
+
+
+const updatePerson = async (id, newPersonData) => {
+    try{
+        let data = await readData("./persons");
+        const person = data.find((person) => person.id == id);
+        const newPerson = {
+            //spread operatorot (...) raboti kako append, ne pravi overwrite, samo dodava/zamenuva
+            ...person,
+            // id: 1
+            // firstName: "Pero"
+            // lastName: "Perovski"
+            ...newPersonData
+            // firstName: "Mile"
+        };
+        data = data.filter((person) => person.id != id); //bez covekot koj probuvame da go azurirame
+        data.push(newPerson);
+        await writeData(data, "./persons");
+    }catch(err){
+        throw err;
+    }
+};
+
+const removePerson = async (id) => {
+    try {
+        const data = await readData("./persons");
+        const out = data.filter((person) => person.id !== id);
+        await writeData(out, "./persons");
+    }catch(err){
+        throw err;
+    }
+}
+
+(async function (){
+    await removePerson(3);
+})();
+
+//Task napravete go istovo (istite metodi koristete gi)
+//za JSON resurs cars.json
+//car {
+// id
+// manufacturer
+// model
+// }
+//BONUS povikajte gi so vistinska funkcija, bez IIFE
