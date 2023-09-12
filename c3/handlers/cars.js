e8bdd8e8b0b0dae3b11d9e7d830f108e91a3ffdc
Viktor Jordanoski <viktorj560@gmail.com>
2023-09-12 21:56:19 +0200

    feat: class 3 end

--- c3/handlers/cars.js
+++ c3/handlers/cars.js
@@ -16,3 +16,65 @@ const {
 // i napravete gi slednive handler funkcii koi ke gi povikuvaat funciite koi gi importiravme od pkg/cars
 // Napravete gi slednive funkcii: getAll, getById, create, update, remove
 // exportirajte gi i importirajte gi vo glavniot vlez na aplikacijata index.js (tamu kade sto ke go definirame express) 
+
+const getAll = async (req, res) => {
+    try{
+        const cars = await getAllCars();
+        return res.status(200).send(cars);
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+const getById = async (req, res) => {
+    try{
+        const id = Number(req.params.id);
+        const car = await getCarById(id);
+        return res.status(200).send(car);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+const create = async (req, res) => {
+    try{
+        await addCar(req.body);
+        return res.status(201).send(req.body);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+const update = async (req, res) => {
+    try{
+        const id = Number(req.params.id);
+        await updateCar(id, req.body);
+        return res.status(204).send("") // Success (OK) but no entity body - update existing document)
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+
+}
+
+const remove = async (req, res) => {
+    try{
+        const id = Number(req.params.id);
+        await removeCar(id);
+        return res.status(200).send("Car deleted!")
+    }catch(err){
+        console.log(err);
+        return res.status(500).send("Internal Server Error");
+    }
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
--- c3/index.js
+++ c3/index.js
@@ -1,5 +1,3 @@
-
-
 // index.js (ovoj fajl kade sto go pisuvame ova) gi setirame rutite, i na tie ruti gi povikuvame handlerite
 // handleri - komuniciraat so serverot i klientot
 // pkg/cars - osnovni funkcii za avtomobili koi go manipuliraat cars.json fajlot
@@ -8,3 +6,31 @@
 // pkg komunicira so handler
 // handler komunicira so index.js (ovoj fajl kade sto ova e napisano i fajlot kade sto go definirame express)
 
+const express = require("express");
+const {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+} = require("./handlers/cars");
+
+const api = express();
+
+api.use(express.json());
+
+//GET
+api.get("/cars", getAll);
+api.get("/cars/:id", getById);
+//POST
+api.post("/cars", create);
+//PUT
+//za ovaa logika nema da ni treba put, ke koristime patch
+//PATCH
+api.patch("/cars/:id", update);
+//DELETE
+api.delete("/cars/:id", remove)
+
+api.listen(10000, (err) => {
+    err ? console.error(err) : console.log("Server started at port 10000!");
+})
